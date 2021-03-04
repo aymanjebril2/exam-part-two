311671626ca189356310b4a2b5d91f82ea48cb80
Ayman Omer <aymangebrilomer@gmail.com>
2021-03-03 21:36:25 -0500

    fix buge

--- src/components/SearchforBreed/SearchDog.js
+++ src/components/SearchforBreed/SearchDog.js
@@ -9,7 +9,8 @@ const SearchDog = () => {
     // let url = `https://dog.ceo/api/breed/${trem}/images`;
     let url = `https://api.thecatapi.com/v1/images/search?breed_ids=${trem}`;
     let response = await axios.get(url).then((res) => {
-      console.log(res);
+      console.log(res.data);
+      setBreed(res.data);
     });
     return response;
   };
@@ -23,6 +24,7 @@ const SearchDog = () => {
     <div>
       <input type="text" onChange={(e) => setTrem(e.target.value)} />
       <button onClick={fatchBreed}> seach</button>
+      <img src={breed.image} alt="search for cat" />
     </div>
   );
 };
